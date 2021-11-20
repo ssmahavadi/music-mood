fcb8e4e1692392efaf7f86454bbf6ed30320b3dd
Soumya Mahavadi <66458097+ssmahavadi@users.noreply.github.com>
2021-11-19 21:25:18 -0500

    Delete data_normalization.py

--- data_normalization.py
+++
@@ -1,8 +0,0 @@
-from sklearn import preprocessing
-import pandas as pd
-audio = pd.read_csv("quantified_dataset.csv")
-names = audio.columns
-d = preprocessing.normalize(audio, axis=0)
-scaled_df = pd.DataFrame(d, columns=names)
-scaled_df.head()
-scaled_df.to_csv("modified_spotify_data.csv")
