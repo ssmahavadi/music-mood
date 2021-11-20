6bf35bf3c6e9617b962b0d7478f55a57a67b831d
Soumya Mahavadi <66458097+ssmahavadi@users.noreply.github.com>
2021-11-19 21:23:42 -0500

    Add files via upload

---
+++ data_normalization.py
@@ -0,0 +1,8 @@
+from sklearn import preprocessing
+import pandas as pd
+audio = pd.read_csv("quantified_dataset.csv")
+names = audio.columns
+d = preprocessing.normalize(audio, axis=0)
+scaled_df = pd.DataFrame(d, columns=names)
+scaled_df.head()
+scaled_df.to_csv("modified_spotify_data.csv")
